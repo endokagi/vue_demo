49411810a23c93ac738d7fcaf5e5f6700e888b9a
Witthawat  Tanoi <38246228+endokagi@users.noreply.github.com>
2021-10-12 16:39:46 +0700

    commit all

---
+++ vue_naja/src/components/test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="hello">
+        <h1>{{ mrg }}</h1>
+     
+        <h2>name</h2>
+        <h2>email</h2>
+        <p>==========================================</p>
+        <h3>wasdwasdwasd</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    props: {
+        mrg: String
+    }
+}
+</script>
+
+<style scoped>
+
+h2 {
+    color: aqua;
+}
+p {
+    color: blue;
+}
+</style>
---
+++ vue_naja/src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dashboard">
+        <Test mrg="Dashboard"/>
+    </div>
+</template>
+
+<script>
+import Test from '@/components/test.vue'
+
+export default ({
+    name: 'Dashboard',
+    components: {
+        Test
+    }
+})
+</script>
